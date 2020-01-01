8804bfd3193e2c67b080eeab400fb54f68a9b7d6
about5211314 <about5211314@qq.com>
2020-01-01 19:42:37 +0800

    测试github提交在钉钉中的提醒

--- student/views.py
+++ student/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):#定义一个函数，第一个参数必须是request
-    return HttpResponse("Hello sl, world. Hello，python.")#返回HttpResonse对象，最终将这行字符显示在页面上
+    return HttpResponse("Hello sl测试git和钉钉提醒, world. Hello，python.")#返回HttpResonse对象，最终将这行字符显示在页面上
 
